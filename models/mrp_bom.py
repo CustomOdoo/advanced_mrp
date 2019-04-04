172a315b8aea46b26d14683d8b38553b16e3de09
athmanziri <issaziri@gmail.com>
2019-04-04 15:52:58 +0300

    [ADD] calculations working

--- models/mrp_bom.py
+++ models/mrp_bom.py
@@ -11,40 +11,24 @@ class MrpBom(models.Model):
     trim = fields.Float('Trim')
     number_of_ups = fields.Integer('Number of Ups (width)')
     product_qty = fields.Float('Target Output')
-    average_final_weight = fields.Float(related='bom_line_ids.average_final_weight', 
-        string='Average Final Weight (grm/pc)', readonly=True, compute='compute_average_final_weight')
-    average_total_weight = fields.Float(related='bom_line_ids.average_total_weight', 
-        string='Average Total Weight (grm/pc)', readonly=True, compute='compute_average_total_weight')
-    average_final_content = fields.Float(related='bom_line_ids.average_final_content',
-        string='Average Dry/Final Content', readonly=True, compute='compute_average_final_content')
-    total_kgs_required = fields.Float(related='bom_line_ids.total_kgs_required',
-        string='Total Kgs Reqired', readonly=True, compute='compute_total_kgs_required')
+    average_total_weight = fields.Float(string='Average Total Weight (grm/pc)', 
+        compute='compute_average_total_weight', readonly=True, store=True)
     is_recipe = fields.Float('Is Test')
     target_output = fields.Float('Target Output', compute='compute_target_output', store=True)
 
     @api.onchange('product_qty')
+    @api.depends('product_qty')
     def compute_target_output(self):
         self.target_output = self.product_qty
     
     @api.onchange('width', 'length')
+    @api.depends('width', 'length')
     def compute_area(self):
         for record in self:
             record.area = record.width * record.length
 
-    @api.onchange('area')
-    @api.depends('bom_line_ids.micron', 'bom_line_ids.product_tmpl_id.density', 'bom_line_ids.coverage')
-    def compute_average_final_weight(self):
-        for record in self.bom_line_ids:
-            record.average_final_weight = (self.area * record.micron * 
-                record.product_tmpl_id.density * record.coverage) / 1000000
-    
-    @api.onchange('average_final_weight', 'target_output')
-    def compute_average_final_content(self):
-        for record in self:
-            if record.average_final_weight and record.average_total_weight:
-                record.average_final_content = (record.average_final_weight / record.average_total_weight) * record.target_output
-
     @api.onchange('bom_line_ids')
+    @api.depends('bom_line_ids')
     def compute_average_total_weight(self):
         for record in self:
             print("############", "AV Total Weight", sum(record.mapped('bom_line_ids.average_final_weight')))
@@ -58,9 +42,12 @@ class MrpBomLine(models.Model):
     density = fields.Float(related='product_tmpl_id.density', string='Density', readonly=True)
     coverage = fields.Float('Coverage', default=100)
     gsm = fields.Float('GSM', compute='compute_gsm', readonly=True)
-    average_final_weight = fields.Float('Average Final Weight (grm/pc)', readonly=True)
-    average_total_weight = fields.Float('Average Total Weight (grm/pc)', readonly=True)
-    average_final_content = fields.Float('Average Dry/Final Content', readonly=True)
+    average_final_weight = fields.Float('Average Final Weight (grm/pc)',
+        compute='compute_average_final_weight', readonly=True)
+    average_total_weight = fields.Float('Average Total Weight (grm/pc)', 
+        related='bom_id.average_total_weight', readonly=True)
+    average_final_content = fields.Float('Average Dry/Final Content',
+        compute='compute_average_final_content', readonly=True)
     total_kgs_required = fields.Float('Total Kgs Reqired', readonly=True)
 
     @api.onchange('micron')
@@ -68,3 +55,17 @@ class MrpBomLine(models.Model):
     def compute_gsm(self):
         for record in self:
             record.gsm = record.micron * record.density * (record.coverage / 100)
+
+    @api.onchange('micron', 'bom_id.area')
+    @api.depends('bom_id.area', 'micron', 'product_tmpl_id.density', 'coverage')
+    def compute_average_final_weight(self):
+        for record in self:
+            record.average_final_weight = (record.bom_id.area * record.micron * 
+                record.product_tmpl_id.density * record.coverage) / 1000000
+
+    @api.onchange('average_final_weight', 'average_total_weight', 'bom_id.target_output')
+    @api.depends('average_final_weight', 'bom_id.target_output', 'average_total_weight')
+    def compute_average_final_content(self):
+        for record in self:
+            if record.average_final_weight and record.average_total_weight:
+                record.average_final_content = (record.average_final_weight / record.average_total_weight) * record.bom_id.target_output
